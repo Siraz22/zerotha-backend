c4dd08757ff52b5c9358bc748c57b7c9ead94b1d
Siraz Naorem <siraz.naorem@neptune-ubi.com>
2023-09-24 14:06:29 +0530

    ZE-02 [feat]: project global constants

---
+++ src/main/java/com/zerodha/backend/persistence/constants/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.zerodha.backend.persistence.constants;
+
+public class GlobalConstants
+{
+    public static String API = "api";
+    public static String VERSION = "1";
+    public static String API_VERSION = API +"/"+VERSION;
+}
