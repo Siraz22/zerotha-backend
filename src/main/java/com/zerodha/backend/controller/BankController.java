8f355c64b90835aa997471e493df5510361ad379
Siraz22 <siraznaorem07@gmail.com>
2023-10-03 21:35:17 +0530

    [develop]: fix request mapping for bank controller and making country dto

--- src/main/java/com/zerodha/backend/controller/BankController.java
+++ src/main/java/com/zerodha/backend/controller/BankController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/bank")
+@RequestMapping("/api/v1/banks")
 public class BankController {
 
     @Autowired
---
+++ src/main/java/com/zerodha/backend/dto/CountryDTO.java
@@ -0,0 +1,14 @@
+package com.zerodha.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CountryDTO {
+    Integer id;
+    String name;
+    String currency;
+}
